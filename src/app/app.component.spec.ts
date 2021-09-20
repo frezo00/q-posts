cbbb28343f28b4edd4eead3e78874a1cef248681
frezo00 <frano.rezo@hotmail.com>
2021-09-20 19:13:24 +0200

    Core: Display plain data on app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,17 +16,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'q-posts'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('q-posts');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('q-posts');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '@core/services/api.service';
+import { Post } from '@shared/models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'q-posts';
+export class AppComponent implements OnInit {
+  posts$!: Observable<Post[]>;
+
+  constructor(private _apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this._apiService.getPosts$();
+  }
 }

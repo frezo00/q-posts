b10802f45a567cd3231f80b2ec3dc84d1600f8f0
frezo00 <frano.rezo@hotmail.com>
2021-09-20 19:08:17 +0200

    Core: Create Shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { CoreModule } from '@core/core.module';
 
 import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouting, CoreModule],
+  imports: [BrowserModule, AppRouting, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })

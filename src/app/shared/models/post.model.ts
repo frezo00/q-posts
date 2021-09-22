4e8ee7fb25ae3420b4eec31eff25e6c79adf705e
frezo00 <frano.rezo@hotmail.com>
2021-09-22 11:10:21 +0200

    Posts: Update Post model & create User and Comment models

--- src/app/shared/models/post.model.ts
+++ src/app/shared/models/post.model.ts
@@ -1,6 +1,17 @@
-export type Post = {
+import { Comment } from './comment.model';
+import { User } from './user.model';
+
+export type PostResponse = {
   id: number;
   userId: number;
   title: string;
   body: string;
+};
+
+export type Post = {
+  id: number;
+  title: string;
+  body: string;
+  user?: User;
+  comments?: Comment[];
 };

5665e34df65d2637d3b60522247c0ac88cbae958
frezo00 <frano.rezo@hotmail.com>
2021-09-24 20:46:20 +0200

    Posts: Install and implement Angular CDK Virtual scroll

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule],
-  exports: [HttpClientModule]
+  imports: [CommonModule, HttpClientModule, ScrollingModule],
+  exports: [HttpClientModule, ScrollingModule]
 })
 export class SharedModule {}
